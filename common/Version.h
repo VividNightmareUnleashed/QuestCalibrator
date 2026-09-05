23918da401414c1225bbdfd3601087293bcd2b27
VividNightmareUnleashed <178581564+VividNightmareUnleashed@users.noreply.github.com>
2026-09-05 23:25:25 +0200

    chore(release): prepare Alpha 1.2

--- common/Version.h
+++ common/Version.h
@@ -3,7 +3,7 @@
 // Included by the .rc files as well as C++, so keep this header free of
 // anything the resource compiler's preprocessor can't handle.
 #define QUESTCAL_VERSION_MAJOR 1
-#define QUESTCAL_VERSION_MINOR 1
+#define QUESTCAL_VERSION_MINOR 2
 #define QUESTCAL_VERSION_PATCH 0
 
-#define QUESTCAL_VERSION_STRING "1.1.0"
+#define QUESTCAL_VERSION_STRING "1.2.0"
